8a11b2ff16859b46bfedb4e824684cb8d8e5d76a
EvilFalcon <ares.sokol@yandex.ru>
2023-02-06 02:48:38 +0300

    rew

---
+++ Black jack/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black_jack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Card
+    {
+        private string _cardSuit;
+        private string _cardValue;
+        public Card(string cardSuit,string cardValue)
+        {
+            _cardSuit=cardSuit;
+            _cardValue=cardValue;
+        }
+        
+    }
+
+    class CardDeck
+    {
+        private int _amountCard = 52;
+        private int _amountCardDeck = 5;
+
+        List<Card> _cardsDesk;
+
+
+
+    }
+
+    class player
+    {
+
+    }
+
+    class CasinoDealer
+    {
+
+    }
+}
