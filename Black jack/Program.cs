e0a41c833583b5b33ebf2264dfffc1b7dcf82772
EvilFalcon <ares.sokol@yandex.ru>
2023-02-07 01:33:45 +0300

    rew

--- Black jack/Program.cs
+++ Black jack/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +11,23 @@ namespace Black_jack
     {
         static void Main(string[] args)
         {
+            Casino casino = new Casino();
+            casino.Play();
+        }
+
+
+    }
+
+    class Casino
+    {
+        private Player _player = new Player();
+        private CasinoDealer _casinoDealer = new CasinoDealer();
+        private List<Card> _card = new List<Card>();
+        public void Play()
+        {
             const string CommandGetCard = "1";
             const string CommandNotTakeCard = "2";
             const string CommandExitGame = "3";
-
-            CasinoDealer casinoDealer = new CasinoDealer();
-            Player player = new Player();
-            CardDeck cardDeck = new CardDeck();
-            cardDeck.CreateDeck();
 
             bool isWork = true;
 
@@ -43,6 +53,42 @@ namespace Black_jack
                 }
             }
         }
+
+
+    }
+
+    class Player
+    {
+        List<Card> _hand = new List<Card>();
+
+        public void TakeCard(Card card) /*добрать карту */
+        {
+            _hand.Add(card);
+        }
+
+        public void BringBackCard(List<Card>  cards)//возврат карт
+        {
+            for (int i = 0; i < _hand.Count; i++)
+            {
+                cards.Add(_hand[i]);
+                _hand.RemoveAt(i);
+            }
+        }
+
+        public void ShowHandInfo()//вывод информации о картах игрока
+        {
+            foreach (Card card in _hand)
+            {
+                card.ShowInfo();
+            }
+        }
+
+    }
+
+    class CasinoDealer
+    {
+        List<Card> hand = new List<Card>();
+        CardDeck deck = new CardDeck();
     }
 
     class Card
@@ -55,27 +101,46 @@ namespace Black_jack
             _cardSuit = cardSuit;
             _cardValue = cardValue;
         }
+        public Card()
+        {
 
+        }
 
+        public int AccruePoints(Card[] card)  /*логика очков за карты */
+        {
+            int cardPoint = 0;
+
+            return cardPoint;
+        }
+
+        public void ShowInfo() /*вывод карт со значением*/
+        {
+            Console.WriteLine($"Масть карты {_cardSuit}, значение карты {_cardSuit}");
+        }
     }
 
     class CardDeck
     {
+        private static Random random = new Random();
+
         private int _amountCardsSuit = 4;
         private int _amountCardsValue = 13;
-        private List<Card> _deck;
+        private List<Card> _deck = new List<Card>();
 
         private List<string> _cardsSuit = new List<string> { "♣", "♠", "♥", "♦" };
         private List<string> _cardsValue = new List<string> { "T", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K" };
 
+        public CardDeck()
+        {
+            FillDeck();
+        }
 
-
-        public void CreateDeck()
+        private void FillDeck()
         {
 
-            for (int i = 0; i < _amountCardsValue; i++)
+            for (int i = 0; i < _cardsValue.Count; i++)
             {
-                for (int j = 0; j < _amountCardsSuit; i++)
+                for (int j = 0; j < _cardsSuit.Count; j++)
                 {
                     Card card = new Card(_cardsValue[i], _cardsSuit[j]);
                     _deck.Add(card);
@@ -83,32 +148,23 @@ namespace Black_jack
                 }
 
             }
+
+            Shuffle();
         }
 
-        public bool TryGetCard(out Card card)
+        private void Shuffle()
         {
-            if (_deck.Count > 0)
+            for (int i = 0; i < _deck.Count; i++)
             {
-                card = _deck[0];
-                _deck.RemoveAt(0);
-                return true;
-            }
-            else
-            {
-                card = null;
-                return false;
-            }
-        }
-        public List<Card> Deck { get; private set; }
-    }
-    class CasinoDealer
-    {
-        private List<Card> _handDealer = new List<Card>();
-    }
+                int indexCard = random.Next(_deck.Count);
+                (_deck[i], _deck[indexCard]) = (_deck[indexCard], _deck[i]);
 
-    class Player
-    {
-        private List<Card> _handPlayer = new List<Card>();
+            }
+
+        }
+
+
+
     }
 
 }
