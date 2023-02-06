ea073d1d108c4502fe3d7ad3b5b856020af420b6
EvilFalcon <ares.sokol@yandex.ru>
2023-02-06 03:04:56 +0300

    rew

--- Black jack/Program.cs
+++ Black jack/Program.cs
@@ -17,21 +17,40 @@ namespace Black_jack
     {
         private string _cardSuit;
         private string _cardValue;
-        public Card(string cardSuit,string cardValue)
+
+        public Card(string cardSuit, string cardValue)
         {
-            _cardSuit=cardSuit;
-            _cardValue=cardValue;
+            _cardSuit = cardSuit;
+            _cardValue = cardValue;
         }
-        
+
     }
 
     class CardDeck
     {
-        private int _amountCard = 52;
-        private int _amountCardDeck = 5;
+        private int _amountCardsSuit = 4;
+        private int _amountCardsValue = 13;
+        List<Card> _deck;
 
-        List<Card> _cardsDesk;
+        private List<string> _cardsSuit = new List<string> { "♣", "♠", "♥", "♦" };
+        private List<string> _cardsValue = new List<string> { "T", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K" };
 
+
+
+        private void CreateDeck()
+        {
+
+            for (int i = 0; i < _amountCardsValue; i++)
+            {
+                for (int j = 0; j < _amountCardsSuit; i++)
+                {
+                    Card card = new Card(_cardsValue[i], _cardsSuit[j]);
+                    _deck.Add(card);
+                    
+                }
+
+            }
+        }
 
 
     }
@@ -43,6 +62,7 @@ namespace Black_jack
 
     class CasinoDealer
     {
+        private int _amountCardDecks = 5;
 
     }
 }
